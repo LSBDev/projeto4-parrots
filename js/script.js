8f4be05af2b14e51edf98fb7fd30fb9f7a005079
profluifel <prof.luifel@gmail.com>
2023-01-17 01:10:57 -0300

    acertando detalhes.

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ function comparador() {
     cartas.innerHTML +=
   `<li>
     <div class="card" onclick="revelarCarta(this)" id ="${i}">
-      <div class="card__front face">
+      <div class="card__front face front">
         <img class="card__img" src="/img/back.png" alt="papagaio">
       </div>
       <div class="card__back face madruguinha">
@@ -48,19 +48,26 @@ function comparador() {
 
 // Ao clicar, revela a carta e compara se tem algo igual a uma carta já virada ou, se for a primeira, espera uma carta ser virada. 
 let cartasViradas = [];
+let cartasFront = [];
 let jogadas = 0;
 let acertos = 0;
 let fimDeJogo = qntCartas/2
 
 function revelarCarta(carta) {
   let cartaVirada = carta.querySelector(".madruguinha");
-  if(cartaVirada.classList.contains("virada")) {
+  let cartaFront = carta.querySelector(".front");
+  if(cartaVirada.parentNode.classList.contains("virada")) {
     return;
   }
   cartaVirada.classList.remove("card__back");
-  cartaVirada.classList.add("virada");
+  cartaVirada.classList.add("card__back--flip");
+  cartaVirada.parentNode.classList.add("virada");
+  cartaFront.classList.remove("card__front");
+  cartaFront.classList.add("card__front--flip");
+  
 
   cartasViradas.push(cartaVirada);
+  cartasFront.push(cartaFront);
   jogadas++;
 
   if(cartasViradas.length == 1) {
@@ -69,14 +76,37 @@ function revelarCarta(carta) {
   }
   if(cartasViradas.length == 2) {
 //CARTAS DIFERENTES!
-    if(cartasViradas[0].querySelector("img"). getAttribute("src") !== cartasViradas[1].querySelector("img").getAttribute("src")) {
-//DESVIRAR APÓS 1 SEGUNDO;
+    if(cartasViradas[0].querySelector("img").getAttribute("src") !== cartasViradas[1].querySelector("img").getAttribute("src")) {
+//DESVIRAR APÓS 1 SEGUNDO
+//DESABILITANDO AS CARTAS
+      // let naoViradas = document.querySelectorAll(".card:not(.virada)"); 
+      // console.log(naoViradas);
+      // naoViradas.forEach((item) => {
+      //   item.setAttribute("disabled", "");
+      // });  
+      
       setTimeout(() => {
-      cartasViradas[0].classList.add("card__back");
-      cartasViradas[0].classList.remove("virada");
-      cartasViradas[1].classList.add("card__back");
-      cartasViradas[1].classList.remove("virada");
-      cartasViradas = [];}, 1000); 
+        // document.querySelector("li:not(.completed):not(.selected)")
+        // naoViradas.forEach((item) => {
+        //   item.removeAttribute("disabled");
+        // });
+        cartasViradas[0].classList.add("card__back");
+        cartasViradas[0].parentNode.classList.remove("virada");
+        cartasViradas[0].classList.remove("card__back--flip");
+        cartasViradas[1].classList.add("card__back");
+        cartasViradas[1].parentNode.classList.remove("virada");
+        cartasViradas[1].classList.remove("card__back--flip");
+
+        cartasFront[0].classList.add("card__front");
+        cartasFront[0].classList.remove("card__front--flip");
+        cartasFront[1].classList.add("card__front");
+        cartasFront[1].classList.remove("card__front--flip");
+
+      
+
+        cartasViradas = [];
+        cartasFront = [];
+      }, 1000); 
       return;
     }
 //CARTAS IGUAIS!!
@@ -85,6 +115,7 @@ function revelarCarta(carta) {
       cartasViradas[0].parentNode.setAttribute("disabled", "");
       cartasViradas[1].parentNode.setAttribute("disabled", "");
       cartasViradas = [];
+      cartasFront = [];
       acertos++;
 
       if(acertos == fimDeJogo) {
@@ -97,6 +128,5 @@ function revelarCarta(carta) {
  
   }
 }
-
 
 //IMPLEMENTAR UM SOM A CADA JOGADA/ACERTO/ERRO/FIMDEJOGO.
